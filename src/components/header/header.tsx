025acca729a8fd596b4a937754f35de7125470b7
Karzan Botani <karzan@botani.nu>
2022-12-16 19:57:17 +0100

    feat(header): add basic layout

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
-import { QwikLogo } from '../icons/qwik';
+import { Link } from '@builder.io/qwik-city';
 import styles from './header.css?inline';
 
 export default component$(() => {
@@ -7,28 +7,16 @@ export default component$(() => {
 
   return (
     <header>
-      <div class="logo">
-        <a href="https://qwik.builder.io/" target="_blank">
-          <QwikLogo />
-        </a>
-      </div>
-      <ul>
-        <li>
-          <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
-            Docs
-          </a>
-        </li>
-        <li>
-          <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
-            Examples
-          </a>
-        </li>
-        <li>
-          <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
-            Tutorials
-          </a>
-        </li>
-      </ul>
+        {/* TODO: SVG */}
+        {/* TODO: Link to earlier date */}
+        <Link href="/">Left</Link>
+
+        {/* TODO: prop */}
+        <span>2022-12-16</span>
+
+        {/* TODO: SVG */}
+        {/* TODO: Link to next date */}
+        <Link href="/">Right</Link>
     </header>
   );
 });
